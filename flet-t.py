63808828523f543a03a55b9e9736aaa16e860de8
ATRCORE-UA <126199526+ATRCORE-UA@users.noreply.github.com>
2026-01-05 09:53:27 +0200

    Update flet-t.py

--- flet-t.py
+++ flet-t.py
@@ -56,7 +56,7 @@ try:
 except: pass
 
 # === КОНФІГУРАЦІЯ ===
-APP_VERSION_NUM = "v14.13" # <--- ВИНОСИМО НОМЕР ВЕРСІЇ ОКРЕМО
+APP_VERSION_NUM = "v14.14" # <--- ВИНОСИМО НОМЕР ВЕРСІЇ ОКРЕМО
 APP_VERSION = f"ATR-TV ({APP_VERSION_NUM} Hotfix)"
 BG_COLOR = "#050505"
 ACCENT_CYAN = "#00F0FF"
@@ -1284,4 +1284,5 @@ if __name__ == "__main__":
 
     assets_path = os.path.join(basedir, "assets")
     
+
     ft.app(target=main, assets_dir=assets_path)
